784f5396b632472991b4e45b4480f6ca3132fede
beks-dyor <bk@dyor.exchange>
2025-02-14 15:49:09 +0600

    transaction confirmation

--- src/pages/SendPage/index.jsx
+++ src/pages/SendPage/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
-import { useSendTransaction } from 'wagmi'
+import { useSendTransaction, useWaitForTransactionReceipt } from 'wagmi'
 import { parseEther } from 'viem'
 
 const SendPage = () => {
     const [recipient, setRecipient] = useState('')
     const [amount, setAmount] = useState('')
 
-    const { data: hash, isPending, sendTransaction } = useSendTransaction()
+    const { data: hash, error, isPending, sendTransaction } = useSendTransaction()
+    const { isLoading: isConfirming, isSuccess: isConfirmed } =
+        useWaitForTransactionReceipt({
+            hash,
+        })
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -17,40 +21,50 @@ const SendPage = () => {
     }
     console.log('hash', hash)
     return (
-        <div className='p-4'>
-            <h1 className='text-xl font-bold mb-4'>Send Transaction</h1>
-            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-                <label>
-                    Recipient Address:
-                    <input
-                        type='text'
-                        placeholder='0x...'
-                        value={recipient}
-                        name={'address'}
-                        onChange={(e) => setRecipient(e.target.value)}
-                        className='border p-2 rounded'
-                        required
-                    />
-                </label>
-                <label>
-                    Amount (ETH):
-                    <input
-                        type='number'
-                        name={'value'}
-                        placeholder='0.01'
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                        className='border p-2 rounded'
-                        required
-                    />
-                </label>
-                <button
-                    type='submit'
-                    disabled={isPending || !sendTransaction}
-                    className='bg-blue-500 text-white px-4 py-2 rounded'>
-                    {isPending ? 'Sending...' : 'Send'}
-                </button>
-            </form>
+        <div>
+            <div className='p-4'>
+                <h1 className='text-xl font-bold mb-4'>Send Transaction</h1>
+                <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                    <label>
+                        Recipient Address:
+                        <input
+                            type='text'
+                            placeholder='0x...'
+                            value={recipient}
+                            name={'address'}
+                            onChange={(e) => setRecipient(e.target.value)}
+                            className='border p-2 rounded'
+                            required
+                        />
+                    </label>
+                    <label>
+                        Amount (ETH):
+                        <input
+                            type='number'
+                            name={'value'}
+                            placeholder='0.01'
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            className='border p-2 rounded'
+                            required
+                        />
+                    </label>
+                    <button
+                        type='submit'
+                        disabled={isPending || !sendTransaction}
+                        className='bg-blue-500 text-white px-4 py-2 rounded'>
+                        {isPending ? 'Sending...' : 'Send'}
+                    </button>
+                </form>
+            </div>
+            <div className={'m'}>
+                {hash && <div>Transaction Hash: {hash}</div>}
+                {isConfirming && <div>Waiting for confirmation...</div>}
+                {isConfirmed && <div>Transaction confirmed.</div>}
+                {error && (
+                    <div>Error: {(error).shortMessage || error.message}</div>
+                )}
+            </div>
         </div>
     )
 }
